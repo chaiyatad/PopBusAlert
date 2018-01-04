1f8ac8defbe841df4846b464a57e1c9654c279d0
Pailin Chitaurjaisuk <pailinchitaurjaisuk@Pailins-MacBook-Air.local>
2018-01-04 18:59:04 +0700

    Basic web page

---
+++ Bas/Thinc/data.js
@@ -0,0 +1,57 @@
+var destination;
+
+var stops=[
+  "Salaprakeaw"
+  ,"Faculty of Political Science"
+  ,"Patumwan Demonstration"
+  ,"Faculty of Veterinary Science"
+  ,"Chaloemphao Junction"
+  ,"MBK Center"
+  ,"Triamudom Suksa School"
+  ,"Faculty of Architecture"
+  ,"Faculty of Arts"
+  ,"Faculty of Engineering"
+  ,"Faculty of Science"
+  ,"Faculty of Education"
+  ,"Charmchuri 9 Building"
+  ,"CU Dharma Centre"
+  ,"Vidhayapattana Building"
+  ,"Metallic and Material Research Institute PetroChemical College"
+  ,"Faculty of Sports Science"
+  ,"CU Dormitory"
+  ,"CU Office"
+  ,"Pop Bus Garage"
+  ,"U-Center"
+  ,"Faculty of Law"
+  ,"Faculty of Medicine"
+  ,"Mahitaladhibesra Building"
+  ,"Mahamakut Building"
+  ,"CU Terrace"
+];
+
+var select = document.getElementById('select');
+var temp= '<option value="" disabled selected>Select your option</option>' ;
+for(var i = 0;i<stops.length;i++){
+  temp+='<option value='+i+'>'+stops[i]+'</option>'
+}
+select.innerHTML = temp
+
+var button = document.getElementById('button');
+button.onclick = function() {
+  destination = select.value;
+  if(destination==0){
+    window.alert("Please select destination");
+  }
+  else{
+    document.cookie = "destination="+destination+";";
+    // document.cookie = "destination1="+destination+";";
+    console.log(document.cookie)
+    location.href='secondScreen.html';
+  }
+
+
+}
+
+  //send cookie
+
+  //send to another page
